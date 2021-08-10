46fdeaac57fa231ad73ca8ba8db5af2bf9476e0b
nikolai.k <nikolai.k@seranking.com>
2021-08-10 11:53:49 +0300

    init repo

---
+++ src/index.js
@@ -0,0 +1,41 @@
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+// import sleep from './sleep';
+
+const app = express();
+
+app.use(cors());
+// async function demo() {
+//   console.log('Taking a break...');
+//   await sleep(2000);
+//   console.log('Two seconds later, showing sleep in a loop...');
+
+//   for (let i = 0; i < 5; i++) {
+//     if (i === 3)
+//       await sleep(2000);
+//     console.log(i);
+//   }
+// }
+// demo();
+
+
+app.get('/', (req, res) => {
+  return res.send('Received a GET HTTP method');
+});
+
+app.post('/', (req, res) => {
+  return res.send('Received a POST HTTP method');
+});
+
+app.put('/', (req, res) => {
+  return res.send('Received a PUT HTTP method');
+});
+
+app.delete('/', (req, res) => {
+  return res.send('Received a DELETE HTTP method');
+});
+
+app.listen(process.env.PORT, () =>
+  console.log(`Example app listening on port ${process.env.PORT}!`),
+);
