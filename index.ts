08eb5e912c3e9754aa2870a6e0a8116725dc332e
Nikolai Kushner <nickolaikushner@gmail.com>
2021-08-11 21:14:40 +0300

    parce html

--- index.ts
+++ index.ts
@@ -1,41 +1,42 @@
-import { Application, Context, Router, helpers } from "https://deno.land/x/oak/mod.ts";
+import {
+  Application,
+  Context,
+  helpers,
+  Router,
+} from "https://deno.land/x/oak/mod.ts";
+import {
+  DOMParser,
+  Element,
+} from "https://deno.land/x/deno_dom/deno-dom-wasm.ts";
 
 const router = new Router();
-// const doc = new DOMParser().parseFromString('', 'text/html');
 
 router
-  .get("/", (context) => {
+  .get("/", (context: Context) => {
     context.response.body = "Hello World!";
   })
   .get("/currency-converter", async (context: Context) => {
-    const url = 'https://myfin.us/currency-converter'
-    const query = helpers.getQuery(context)
+    const url = "https://myfin.us/currency-converter";
+    const query = helpers.getQuery(context);
 
-    const from = query.from || 'usd'
-    const to = query.to || 'usd'
-    const amount = query.amount || 1
+    const from = query.from || "usd";
+    const to = query.to || "usd";
+    const amount = query.amount || 1;
 
     try {
-      const res = await fetch(`${url}/${from}-${to}/${amount}`)
-      const html = await res.text();
-
-      console.log(html); // TODO parce html
-
-      // const $ = cheerio.load(response.body)
-      // const currency = $('.conversion__value-text span:eq(1)').html()
-
-      // return res.json(currency ? currency.trim() : 'no element')
-
-      context.response.body = {from, to, amount} || "no element";
+      const res = await fetch(`${url}/${from}-${to}/${amount}`);
+      const text = await res.text();
+      const html = new DOMParser().parseFromString(text, "text/html")!;
+      const value = html.querySelector(".conversion__value-text")!;
+      context.response.body = value
+        ? `${value.textContent.replace("  ", " ")}\n`
+        : "no data";
     } catch (e) {
       context.response.body = "error";
     }
-
-  })
+  });
 
 const app = new Application();
-
 app.use(router.routes());
 app.use(router.allowedMethods());
-
 await app.listen({ port: 3000 });
